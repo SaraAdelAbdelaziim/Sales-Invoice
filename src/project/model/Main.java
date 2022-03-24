4fbb82c44d21010743941b1c3522ec03944cc3ea
Sara Mohamed <sara.abdelaziim@gmail.com>
2022-03-24 08:03:07 +0300

    initial

---
+++ src/project/model/Main.java
@@ -0,0 +1,10 @@
+package project.model;
+import project.view.SalesInvoiceFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      new SalesInvoiceFrame("Design Preview [NewJFrame]").setVisible(true);
+    }
+}
